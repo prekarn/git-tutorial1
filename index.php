15ec2feace342c5ee618c1dced93166fb5e3ea2d
prekarn <prekarn@mut.ac.th>
2024-07-05 17:01:34 +0700

    commit3

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
     echo "<h1>My repository</h1>" 
     echo "<h1>My name is aj yai</h1>" 
+    echo "<h1>My name is Doraemon</h1>" 
     ?>
--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 <?php
-    echo "<h1>Main</h1>";
-    echo "<h1>Product</h1>";
-    echo "<h1>About us</h1>";
-    echo "<h1>Map</h1>";
+    echo "<h1>หน้าหลัก</h1>";
+    echo "<h1>สินค้า</h1>";
+    echo "<h1>เกี่ยวกับเรา</h1>";
+    echo "<h1>แผนที่</h1>";
 ?>
