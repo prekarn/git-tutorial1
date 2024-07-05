06f8cda4361e9498aaf965c89a4a87374da472d9
prekarn <prekarn@mut.ac.th>
2024-07-05 17:11:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,7 @@
     echo "<h1>My repository</h1>" 
     echo "<h1>My name is aj yai</h1>" 
     echo "<h1>My name is Doraemon</h1>" 
+    echo "<h1>My repository</h1>" 
+    echo "<h1>My name is aj yai</h1>" 
+    echo "<h1>My name is Doraemon</h1>" 
     ?>
